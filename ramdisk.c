a7f516d671f26a3d9e81b5e41d4cf49bb0915ec2
zzt <qq344696734@gmail.com>
2023-01-08 20:51:29 +0800

    Update ramdisk.c

--- ramdisk.c
+++ ramdisk.c
@@ -1,160 +1,155 @@
-#include <linux/module.h>
 #include <linux/init.h>
-#include <linux/kernel.h>
+#include <linux/module.h>
 #include <linux/blkdev.h>
-#include <linux/blk_types.h>
-#include <linux/device.h>
-#include <linux/blk-mq.h>
-#include <linux/list.h>
+#include <linux/bio.h>
+#include <linux/pagemap.h>
 
-static int dev_major = 0;
+static int major = 0;
+static unsigned char memory_data[128 * 1024 * 1024];
 
-struct block_dev {
-    sector_t capacity;
-    u8 *data;
-    struct blk_mq_tag_set tag_set;
-    struct request_queue *queue;
-    struct gendisk *gdisk;
+/*
+ * Copy n bytes from src to the brd starting at sector. Does not sleep.
+ */
+static void copy_to_brd(const void *src, sector_t sector, size_t n)
+{
+	memcpy(memory_data + (sector << 9), src, n);
+}
+
+/*
+ * Copy n bytes to dst from the brd starting at sector. Does not sleep.
+ */
+static void copy_from_brd(void *dst, sector_t sector, size_t n)
+{
+	memcpy(dst, memory_data + (sector << 9), n);
+}
+
+/*
+ * Process a single bvec of a bio.
+ */
+static int brd_do_bvec(struct page *page, unsigned int len, unsigned int off, unsigned int op, sector_t sector)
+{
+	void *mem;
+	int err = 0;
+
+	mem = kmap_atomic(page);
+	if (!op_is_write(op)) {
+		copy_from_brd(mem + off, sector, len);
+		flush_dcache_page(page);
+	} else {
+		flush_dcache_page(page);
+		copy_to_brd(mem + off, sector, len);
+	}
+	kunmap_atomic(mem);
+
+	return err;
+}
+
+static blk_qc_t brd_submit_bio(struct bio *bio)
+{
+	sector_t sector = bio->bi_iter.bi_sector;
+	struct bio_vec bvec;
+	struct bvec_iter iter;
+
+	bio_for_each_segment(bvec, bio, iter) {
+		unsigned int len = bvec.bv_len;
+		int err;
+
+		/* Don't support un-aligned buffer */
+		WARN_ON_ONCE((bvec.bv_offset & (SECTOR_SIZE - 1)) ||
+				(len & (SECTOR_SIZE - 1)));
+
+		err = brd_do_bvec(bvec.bv_page, len, bvec.bv_offset,
+				  bio_op(bio), sector);
+		if (err)
+			goto io_error;
+		sector += len >> SECTOR_SHIFT;
+	}
+
+	bio_endio(bio);
+	return BLK_QC_T_NONE;
+io_error:
+	bio_io_error(bio);
+	return BLK_QC_T_NONE;
+}
+
+static int brd_rw_page(struct block_device *bdev, sector_t sector, struct page *page, unsigned int op)
+{
+	int err;
+
+	if (PageTransHuge(page))
+		return -ENOTSUPP;
+	err = brd_do_bvec(page, PAGE_SIZE, 0, op, sector);
+	page_endio(page, op_is_write(op), err);
+	return err;
+}
+
+static const struct block_device_operations brd_fops = {
+	.owner =		THIS_MODULE,
+	.submit_bio =		brd_submit_bio,
+	.rw_page =		brd_rw_page,
 };
 
-static struct block_dev *block_device = NULL;
+unsigned long rd_size = CONFIG_BLK_DEV_RAM_SIZE;
 
-static struct block_device_operations blockdev_ops = {
-    .owner = THIS_MODULE
-};
+static struct gendisk *disk;
 
-static int do_request(struct request *rq, unsigned int *nr_bytes) {
-    int ret = 0;
-    struct bio_vec bvec;
-    struct req_iterator iter;
-    struct block_dev *dev = rq->q->queuedata;
-    loff_t pos = blk_rq_pos(rq) << SECTOR_SHIFT;
-    loff_t dev_size = (loff_t)(dev->capacity << SECTOR_SHIFT);
+static int brd_alloc(void)
+{
+	disk = blk_alloc_disk(NUMA_NO_NODE);
+	if (!disk)
+		goto out_free_dev;
 
-    rq_for_each_segment(bvec, rq, iter) {
-        unsigned long b_len = bvec.bv_len;
+	disk->major		= major;
+	disk->first_minor	= 0;
+	disk->minors		= 1;
+	disk->fops		= &brd_fops;
+	disk->flags		= GENHD_FL_EXT_DEVT;
+    strcpy(disk->disk_name, "ramdisk");
+	set_capacity(disk, rd_size * 2);
+	
+	blk_queue_physical_block_size(disk->queue, PAGE_SIZE);
+	blk_queue_flag_set(QUEUE_FLAG_NONROT, disk->queue);
+	blk_queue_flag_clear(QUEUE_FLAG_ADD_RANDOM, disk->queue);
+	add_disk(disk);
 
-        void* b_buf = page_address(bvec.bv_page) + bvec.bv_offset;
+	return 0;
 
-        if ((pos + b_len) > dev_size) {
-            b_len = (unsigned long)(dev_size - pos);
-        }
-
-        if (rq_data_dir(rq) == WRITE) {
-            memcpy(dev->data + pos, b_buf, b_len);
-        } else {
-            memcpy(b_buf, dev->data + pos, b_len);
-        }
-
-        pos += b_len;
-        *nr_bytes += b_len;
-    }
-
-    return ret;
+out_free_dev:
+	return -ENOMEM;
 }
 
-static blk_status_t queue_rq(struct blk_mq_hw_ctx *hctx, const struct blk_mq_queue_data* bd) {
-    unsigned int nr_bytes = 0;
-    blk_status_t status = BLK_STS_OK;
-    struct request *rq = bd->rq;
+static int __init brd_init(void)
+{
+	int err;
 
-    blk_mq_start_request(rq);
+    major = register_blkdev(major, "ramdisk");
+	if (major <= 0)
+		return -EIO;
 
-    if (do_request(rq, &nr_bytes) != 0) {
-        status = BLK_STS_IOERR;
-    }
+	err = brd_alloc();
+	if (err)
+		goto out_free;
 
-    if (blk_update_request(rq, status, nr_bytes)) {
-        BUG();
-    }
+	pr_info("ramdisk: module loaded\n");
+	return 0;
 
-    __blk_mq_end_request(rq, status);
+out_free:
+	unregister_blkdev(major, "ramdisk");
 
-    return status;
+	pr_info("ramdisk: module NOT loaded !!!\n");
+	return err;
 }
 
-static struct blk_mq_ops mq_ops = {
-    .queue_rq = queue_rq,
-};
+static void __exit brd_exit(void)
+{
+	unregister_blkdev(major, "ramdisk");
 
-static int __init ramdisk_init(void) {
-    dev_major = register_blkdev(dev_major, KBUILD_MODNAME);
+	del_gendisk(disk);
+	blk_cleanup_disk(disk);
 
-    if (dev_major <= 0) {
-        pr_err("Unable to get major number\n");
-        return -EBUSY;
-    }
-
-    block_device = kmalloc(sizeof(struct block_dev), GFP_KERNEL);
-
-    if (block_device == NULL) {
-        pr_err("Failed to allocate struct block_device\n");
-        unregister_blkdev(dev_major, KBUILD_MODNAME);
-        return -ENOMEM;
-    }
-
-    block_device->capacity = (16 * 1024 * 1024) >> 9;
-
-    block_device->data = __vmalloc(block_device->capacity << 9, GFP_NOIO | __GFP_ZERO);
-
-    if (block_device->data == NULL) {
-        pr_err("Failed to allocate device IO buffer\n");
-        unregister_blkdev(dev_major, KBUILD_MODNAME);
-        kfree(block_device);
-        return -ENOMEM;
-    }
-
-    pr_info("Initializing queue\n");
-
-    block_device->queue = blk_mq_init_sq_queue(&block_device->tag_set, &mq_ops, 128, BLK_MQ_F_SHOULD_MERGE);
-
-    if (block_device->queue == NULL) {
-        pr_err("Failed to allocate device queue\n");
-        vfree(block_device->data);
-
-        unregister_blkdev(dev_major, KBUILD_MODNAME);
-        kfree(block_device);
-
-        return -ENOMEM;
-    }
-
-    block_device->queue->queuedata = block_device;
-
-    block_device->gdisk = alloc_disk(1);
-    block_device->gdisk->flags = GENHD_FL_NO_PART_SCAN;
-    block_device->gdisk->major = dev_major;
-    block_device->gdisk->first_minor = 0;
-    block_device->gdisk->fops = &blockdev_ops;
-    block_device->gdisk->queue = block_device->queue;
-    block_device->gdisk->private_data = block_device;
-    strcpy(block_device->gdisk->disk_name, "my_ramdisk");
-
-    // 设置设备名称，会在 /dev 目录下生成 my_ramdisk 设备文件
-    pr_info("Adding disk %s\n", block_device->gdisk->disk_name);
-    set_capacity(block_device->gdisk, block_device->capacity);
-    add_disk(block_device->gdisk);
-
-    return 0;
+	pr_info("brd: module unloaded\n");
 }
 
-static void __exit ramdisk_exit(void) {
-    if (block_device->gdisk) {
-        del_gendisk(block_device->gdisk);
-        put_disk(block_device->gdisk);
-    }
-
-    if (block_device->queue) {
-        blk_cleanup_queue(block_device->queue);
-    }
-
-    vfree(block_device->data);
-
-    unregister_blkdev(dev_major, KBUILD_MODNAME);
-    kfree(block_device);
-}
-
-module_init(ramdisk_init);
-module_exit(ramdisk_exit);
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("zhaozhentao");
-MODULE_DESCRIPTION("ramdisk module");
+module_init(brd_init);
+module_exit(brd_exit);
